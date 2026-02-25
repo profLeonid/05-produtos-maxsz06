5fbc67324505e772d9b13cdb6f9f64e2c10be75f
MAXWILLIAN ALBINO ZACARIAS DE SANTANA <25203648@CFP127ALUNO.LOCAL>
2026-02-25 07:43:58 -0300

    Tabela (em andamento)

--- main.js
+++ main.js
@@ -1,44 +1,48 @@
 `use strict`
 
-function adicionarProduto(){
+function adicionarProduto() {
 
     const lista = document.getElementById(`lista`)
     const produto = document.getElementById(`produto`)
     const codigo = document.getElementById(`codigo`)
     const quantidade = document.getElementById(`quantidade`)
 
-    if(produto.value.trim()===""){
-        alert ("Complete as caixas")
+    if (produto.value.trim() === "") {
+        alert("Complete as caixas")
         e.preventDefault()
         return
-    }else if(codigo.value.trim === ""){
-        alert ("Complete as caixas")
+    } else if (codigo.value.trim === "") {
+        alert("Complete as caixas")
         e.preventDefault()
         return
-    }else if (quantidade.value.trim() == ""){
-        alert ("Complete as caixas")
+    } else if (quantidade.value.trim() == "") {
+        alert("Complete as caixas")
         e.preventDefault()
         return
     }
 
 
-    const tableAtributos = document.getElementById(`atributos`)
+    const tr = document.createElement("tr")
+    tr.className = "bg-white shadow-md rounded-lg hover:shadow-lg hover:bg-gray-50 transition";
+    tr.innerHTML = 
+        <td class="px-4 py-4 rounded-l-lg font-semibold">${}</td>
 
-    
+
+
 
     lista.appendChild(span)
 }
 
 function removerProduto() {
     const lista = document.getElementById('lista') // pega a lista com os elementos dentro 
-  
+
     // pega o último elemento adicionado
     const ultimoProduto = lista.lastElementChild
-  
+
     // se existir, remove
     if (ultimoProduto) {
-      ultimoProduto.remove()
-    }else{
+        ultimoProduto.remove()
+    } else {
         alert("Nenhum Elemento Para Excluir")
     }
-  }
+}
